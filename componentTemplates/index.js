fdb2208ac903a475b190d50c5848ed1c3eca7fc5
David <david.j.roberts303@gmail.com>
2018-10-13 11:29:18 +0100

    Updated to include generating index.js files for provided directory

--- componentTemplates/index.js
+++ componentTemplates/index.js
@@ -10,12 +10,36 @@ const generateStringFromJs = (component, componentName, cssLine) => {
     return componentStr;
 }
 
-const componentTemplates = (component, componentName, cssLine) => {
+generateIndexFile = (root) => {
+    let files = fs.readdirSync(root);
+    files = files.filter((file) => !file.includes('.css') && !file.includes('index.js'));
+
+    //if there is only one file in the directory use single index
+    if (files.length === 1){
+        let componentStr = fs.readFileSync(path.join(__dirname, 'singleComponentIndex.js'), 'utf-8');
+        componentStr = componentStr.replace(/__COMPONENT_NAME__/g, files[0].replace(/\.js$/, ''));
+        return componentStr;
+    //else use the multi component file (loop over)
+    } else if (files.length > 1){
+        let index = '';
+        let componentStr = fs.readFileSync(path.join(__dirname, 'multiComponentIndex.js'), 'utf-8');
+        for (let i = 0; i < files.length; i++){
+                index = index + componentStr.replace(/__COMPONENT_NAME__/g, files[i].replace(/\.js/, '')) + '\n';
+        }
+        return index;
+    } else {
+        throw new Errow('You don\'t have any javascript files in that directory');
+    }
+}
+
+const componentTemplates = (component, componentName, cssLine, root) => {
     switch (component) {
-        case "pure":
+        case 'pure':
             return generateStringFromJs(component, componentName, cssLine);
-        case "class":
+        case 'class':
             return generateStringFromJs(component, componentName, cssLine);
+        case 'index':
+            return generateIndexFile(root);
     }
 }
 
--- index.js
+++ index.js
@@ -16,8 +16,9 @@ var program = require('commander')
   .option('-p, --pure', 'Create Pure Function Component')
   .option('-k --class', 'Create Class Component')
   .option('-c, --css', `Add ${componentName}.css`)
+  .option('-i, --index', 'Create index.js file for all components in supplied directory')
   .parse(process.argv)
-
+ 
 createComponent(componentName);
 
 function createComponent(name) {
@@ -35,8 +36,10 @@ function createComponent(name) {
     )
     cssLine = `import from './${name}.css'\n\n`
   }
-
-  if (program.pure) {
+  if (program.index){
+    const template = componentTemplates('index', null, null, root);
+    fs.writeFileSync(path.join(root, 'index.js'), template);
+  } else if (program.pure) {
     const template = componentTemplates('pure', name, cssLine);
     fs.writeFileSync(
       path.join(root, `${name}.js`),
